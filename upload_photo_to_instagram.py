a58c335e7413fefa57821039303552072e375261
antauren <antauren@yandex.ru>
2019-02-03 03:08:05 +0300

    вынес переменные env за пределы ф-и

--- upload_photo_to_instagram.py
+++ upload_photo_to_instagram.py
@@ -7,19 +7,19 @@ from utils import get_files_path_from_dir, get_ext
 IMG_EXT_SET = {'.png', '.jpg', '.jpeg', '.gif', '.tif'}
 
 
-def upload_photo_to_instagram(img_dir):
+def upload_photo_to_instagram(img_dir, username, password):
     files = get_files_path_from_dir(img_dir)
 
     image_files = filter(lambda path: get_ext(path).lower() in IMG_EXT_SET, files)
 
-    variables = get_variables('.env')
-
     bot = Bot()
-    bot.login(username=variables['USERNAME'], password=variables['PASSWORD'])
+    bot.login(username, password)
 
     for img in image_files:
         bot.upload_photo(img)
 
 
 if __name__ == '__main__':
-    upload_photo_to_instagram('images')
+    variables = get_variables('.env')
+
+    upload_photo_to_instagram('images', variables['USERNAME'], variables['PASSWORD'])
